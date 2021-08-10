902fcdb3f2961c7e3390f48e2519e1cda1723c08
meng.chen <meng.chen@17zuoye.com>
2021-08-10 23:30:38 +0800

    添加说明

--- src/static/common/desktopCapturer.js
+++ src/static/common/desktopCapturer.js
@@ -2,13 +2,20 @@
  * 渲染进程：录屏
  * 1、通过 electron desktopCapturer 和 navigator 获取要进行录制的媒体源信息；
  * 2、使用 MediaRecorder 对视频流进行录制；
- * 3、将 MediaRecorder 保存到本地文件。
+ *    MediaStream 将要录制的流,它可以是来自于使用 navigator.mediaDevices.getUserMedia() 
+ *    创建的流或者来自于 <audio>, <video> 以及 <canvas> DOM元素.
+ * 3、HTML5的FileReader API可以让客户端浏览器对用户本地文件进行读取，
+ *    这样就不再需要上传文件由服务器进行读取了，这大大减轻了服务器的负担，
+ *    也节省了上传文件所需要的时间。
+ *    不过在实践中我发现用FileReader.readAsText()可以轻易地处理一个300k的日志文件，
+ *    但当日志文件有1G、甚至2G那么大，浏览器就会崩溃。这是因为readAsText()会一下子把目标文件加载至内存，
+ *    导致内存超出上限。所以如果Web应用常常需要处理大文件时，
+ *    我们应该使用FileReader.readAsArrayBuffer()来一块一块读取文件。
+ * 4、fs.writeFile保存到本地文件
  */
 const { desktopCapturer } = require('electron')
 const fs = require("fs");
 const path = require('path');
-const { addAbortSignal } = require('stream');
-
 
 var startRecording = async function(screenName = "Webview"){
   desktopCapturer.getSources({ types: ['window', 'screen'] }).then(async sources => {
